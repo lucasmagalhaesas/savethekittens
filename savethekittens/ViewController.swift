7b069b29a3e609904fc689cc57039e2f6e06d229
lucasm <lucasmagalhaesas@gmail.com>
2017-06-22 12:07:55 -0300

    Initial Commit

---
+++ savethekittens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  savethekittens
+//
+//  Created by Lucas Magalhães on 22/06/17.
+//  Copyright © 2017 Lucas Magalhães. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
